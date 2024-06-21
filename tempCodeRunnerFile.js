faf764f753b40ad05483b42b4dd258696aeae5b5
ZakirKhunkaev <zakirkhunkaev@zakirkhunkaev@gmail.com>
2024-06-21 12:07:12 +0300

    find the middle element solution

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,18 +1,6 @@
-function incrementString(string) {
-  const strToArr = string.split('');
-  const forNum = [];
-
-  const lastLetter = string[string.length - 1];
-
-  for (let i = 0; i < strToArr.length; i++) {
-    if (parseInt(strToArr[i])) {
-      forNum.push(strToArr[i]);
-      delete strToArr[i];
-    }
-  }
-  const endSum = Number(forNum.join('')) + 1;
-
-  strToArr.push(endSum);
-
-  return strToArr.join('');
+function gimme(triplet) {
+  let min = Math.min(...triplet);
+  let max = Math.max(...triplet);
+  let middle = triplet.filter((item) => item > min && item < max);
+  return triplet.indexOf(Number(middle.join('')));
 }
