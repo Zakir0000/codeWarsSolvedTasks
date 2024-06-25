8ca91939fdf891789408a8327a758fca06b3e2e8
ZakirKhunkaev <zakirkhunkaev@zakirkhunkaev@gmail.com>
2024-06-25 15:49:35 +0300

    unique in Order

---
+++ multiplicationTable.js
@@ -0,0 +1,14 @@
+multiplicationTable = function (size) {
+  let result = [];
+
+  for (let i = 0; i < size; i++) {
+    result[i] = [];
+    for (let j = 0; j < size; j++) {
+      result[i][j] = (i + 1) * (j + 1);
+    }
+  }
+
+  return result;
+};
+
+console.log(multiplicationTable(3));
